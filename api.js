3b3eda1f2f1de105ba898d863e0fd480a82a0419
PapaRhino <148014686+PapaRhino@users.noreply.github.com>
2025-01-20 23:01:59 -0700

    Delete api.js

--- api.js
+++
@@ -1,7 +0,0 @@
-async function getPokemonData(pokedexNum) {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokedexNum}`);
-    if (!response.ok) throw new Error(`Failed to fetch data for Pokédex #${pokedexNum}`);
-    return await response.json();
-}
-
-export { getPokemonData };
